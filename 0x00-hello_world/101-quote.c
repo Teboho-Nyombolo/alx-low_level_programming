21f0ccfdeb537e93d17f6f321ca15e8f05c756a4
TatakaSibu <thulaninyombolo@gmail.com>
2022-09-08 13:37:20 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,7 @@
  *
  * Return: Always 1 (Success)
  */
-
 int main(void)
-
 {
 
 	write(2, "and that piece of art is useful\" - Dora Korpar,
@@ -15,5 +13,5 @@ int main(void)
 			2015-10-19\n", 59);
 
 	return (1);
-
 }
+
