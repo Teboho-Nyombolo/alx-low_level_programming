22e43ad02945b3d11dc735fcb3c158e4b1f66915
TatakaSibu <thulaninyombolo@gmail.com>
2022-09-08 14:58:11 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	int 59 = s("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n")
 	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
