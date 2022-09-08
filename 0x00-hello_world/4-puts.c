b1a8b09d5526f2ac796f2bbaaee662a41dbb12ca
TatakaSibu <thulaninyombolo@gmail.com>
2022-09-08 04:57:38 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+ {puts("\"Programming is like building a multilingual puzzle");
 	return(0);
-}
+ }
