8cb4f7fc667fd7a3286a0cdde4ab8c4d9060d5e5
TatakaSibu <thulaninyombolo@gmail.com>
2022-09-12 16:22:30 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char f;
+
+	c = 'a';
+	f = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++
+		}
+	while
+		(f <= 'Z') {
+			putchar(f);
+			f++
+		}
+	putchar('\n');
+	return (0);
+}
