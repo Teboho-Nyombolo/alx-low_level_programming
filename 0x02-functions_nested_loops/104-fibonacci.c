a3e5d97e8d415110fac32ee08d7ae33452996a3b
TatakaSibu <thulaninyombolo@gmail.com>
2022-09-14 14:30:03 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,20 +8,14 @@
 int main(void)
 {
 	int count;
-
 	unsigned long fib1 = 0, fib2 = 1, sum;
-
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-
 	unsigned long half1, half2;
-
 
 	for (count = 0; count < 92; count++)
 	{
 		sum = fib1 + fib2;
 		printf("%lu, ", sum);
-
-
 		fib1 = fib2;
 		fib2 = sum;
 	}
@@ -39,11 +33,9 @@ int main(void)
 			half1 += 1;
 			half2 %= 10000000000;
 		}
-
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(", ");
-
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
