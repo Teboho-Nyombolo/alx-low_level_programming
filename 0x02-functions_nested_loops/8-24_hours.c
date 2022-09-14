22acaf3aba4b625b04b970650a69a81939ab42a9
TatakaSibu <thulaninyombolo@gmail.com>
2022-09-14 03:13:49 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,8 +8,8 @@
  */
 void jack_bauer(void)
 {
-	int hours - 0;
-	int minutes - 0;
+	int hours = 0;
+	int minutes = 0;
 	int hours_remainder;
 	int mins_remainder;
 
@@ -17,8 +17,8 @@ void jack_bauer(void)
 	{
 		while (minutes <= 59)
 		{
-			mins_remainder - minutes % 10;
-			hours_remainder - hours % 10;
+			mins_remainder = minutes % 10;
+			hours_remainder = hours % 10;
 			_putchar(hours / 10 + '0');
 			_putchar(hours_remainder + '0');
 			_putchar(':');
