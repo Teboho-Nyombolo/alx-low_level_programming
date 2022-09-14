275892ef925013f5efa570d7f5874eba8d321e33
TatakaSibu <thulaninyombolo@gmail.com>
2022-09-14 03:00:35 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - function that prints every minute of the day of Jack Bauer, starti
+ * jack_bauer - function that prints every minute of the day of Jack Bauer.
  *
  * n from 00:00 to 23:59, min loop counts mins, while hour counts hours
  * and resets mins
