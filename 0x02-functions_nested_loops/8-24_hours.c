5c5d7924dd4bf94f371bc59c24b3267f21c84c70
TatakaSibu <thulaninyombolo@gmail.com>
2022-09-14 03:28:23 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -23,7 +23,7 @@ void jack_bauer(void)
 			_putchar(hours_remainder + '0');
 			_putchar(':');
 			_putchar(minutes / 10 + '0');
-			_putchar(minutes_remainder + '0');
+			_putchar(mins_remainder + '0');
 			minutes++;
 			_putchar('\n');
 		}
