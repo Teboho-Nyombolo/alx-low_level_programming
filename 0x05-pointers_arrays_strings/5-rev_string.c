37e463631dea6854b8515501d3dd41232a5c138f
TatakaSibu <thulaninyombolo@gmail.com>
2022-09-19 15:08:34 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -17,3 +17,17 @@ void rev_string(char *s)
 		*(s + len - i - 1) = tmp;
 	}
 }
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: the length of the given string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
