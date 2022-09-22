65dfbe1c42efb820dd693c26d121a822a20e9fe7
TatakaSibu <thulaninyombolo@gmail.com>
2022-09-22 02:47:45 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 
 /**
  * strcat - Concatenates the string pointed to by @src, including the terminat
- * null byte, to the end of the string pointed to by @dest.
+ *          null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
