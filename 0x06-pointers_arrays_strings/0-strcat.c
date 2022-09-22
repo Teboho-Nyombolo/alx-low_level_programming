cd25fd7cc6f9956bcfb157ba6432bf14a06dc574
TatakaSibu <thulaninyombolo@gmail.com>
2022-09-22 02:26:17 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,8 +14,9 @@ char *strcat(char *dest, const char *src)
 
 	while (dest[index++])
 		dest_len++;
-
+	
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
+	
 	return (dest);
 }
