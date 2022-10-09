921dbf94445ef2da617699f891baa926635d0835
TatakaSibu <thulaninyombolo@gmail.com>
2022-10-09 08:04:25 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,6 @@
  * initializes it with a specific char.
  * @size: Size of the array
  * @c: Character to insert
- *
  * Return: NULL if size is zero or if it fails,
  * pointer to array if everything is normal.
  */
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdlib.h>
 
 int _putchar(char);
